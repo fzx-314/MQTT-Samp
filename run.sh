cea6e6a6343907e5b062a960e3ed57ad926b7896
Keval Malde <maldekeval@ymail.com>
2021-02-06 15:03:10 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gnome-terminal -e "python client.py client1"
+gnome-terminal -e "python client.py client2"
+gnome-terminal -e "python client.py client3"
+gnome-terminal -e "python client.py client4"
+gnome-terminal -e "python client.py client5"
